9b85483e2c14951d38c5e8bba629d6730860b3f9
Bogdan Nitescu <nitescubgdn@gmail.com>
2026-02-22 22:53:20 +0000

    Initial Commit

---
+++ LightsUp/LightsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightsUpApp.swift
+//  LightsUp
+//
+//  Created by Bogdan Nitescu on 22/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct LightsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LightsUpTests/LightsUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  LightsUpTests.swift
+//  LightsUpTests
+//
+//  Created by Bogdan Nitescu on 22/02/2026.
+//
+
+import Testing
+@testable import LightsUp
+
+struct LightsUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
